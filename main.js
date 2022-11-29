d4ffa3cd0f8c7f7dc982f014685db6ea50c3fbf6
Rohuu <rohuu99022@gmail.com>
2022-11-29 11:43:11 +0530

    added modal for give-away & added pet-data in pet-list

--- main.js
+++ main.js
@@ -6,74 +6,39 @@ $(function () {
 
     var responseData = [
         {
-            "_id": "63850949ec2830dc9f065903",
-            "age": 20,
-            "name": "Leila Aguirre",
-            "gender": "female",
-            "company": "DANCERITY"
+            "age": 12,
+            "name": "Lyka",
+            "breed": "Dasch",
         },
         {
-            "_id": "638509495017858500279bd1",
-            "age": 25,
-            "name": "Lott Schmidt",
-            "gender": "male",
-            "company": "WAAB"
+            "age": 3,
+            "name": "Albie",
+            "breed": "Beagle",
         },
         {
-            "_id": "638509493c057689a66bd2c5",
-            "age": 25,
-            "name": "Matthews Adkins",
-            "gender": "male",
-            "company": "TELLIFLY"
+            "age": 5,
+            "name": "Happy",
+            "breed": "Lab",
         },
         {
-            "_id": "63850949ef395ad3c8700989",
-            "age": 22,
-            "name": "Vicky Sloan",
-            "gender": "female",
-            "company": "GYNKO"
+            "age": 6,
+            "name": "Ramu",
+            "breed": "Golden Retriever",
         },
         {
-            "_id": "63850949a416c427a73f5604",
-            "age": 28,
-            "name": "Saundra Wagner",
-            "gender": "female",
-            "company": "ELECTONIC"
+            "age": 7,
+            "name": "Pinky",
+            "breed": "Lab",
         },
         {
-            "_id": "63850949b069934e57ecf486",
-            "age": 29,
-            "name": "Yesenia Case",
-            "gender": "female",
-            "company": "TOURMANIA"
+            "age": 21,
+            "name": "Sweetie",
+            "breed": "Beagle",
         },
         {
-            "_id": "6385094902e61fbd03e233b5",
-            "age": 22,
-            "name": "Weaver Solomon",
-            "gender": "male",
-            "company": "VERBUS"
-        },
-        {
-            "_id": "63850949c940c5b68bb83fb6",
-            "age": 24,
-            "name": "Sharp Tanner",
-            "gender": "male",
-            "company": "EXODOC"
-        },
-        {
-            "_id": "63850949f0ea21e1ead73389",
-            "age": 35,
-            "name": "Rosario Mcmillan",
-            "gender": "female",
-            "company": "FOSSIEL"
-        },
-        {
-            "_id": "638509492520a96034a70b57",
-            "age": 25,
-            "name": "Newton Ashley",
-            "gender": "male",
-            "company": "SYNKGEN"
+            "age": 11,
+            "name": "Blacky",
+            "breed": "Dasch",
         }
     ];
     $table.bootstrapTable({ data: responseData })
